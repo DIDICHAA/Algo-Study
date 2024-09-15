da9619d850c38fef3d8c5d6ba79e4439b58b9498
Jieun Seo <127473767+DIDICHAA@users.noreply.github.com>
2024-09-16 02:06:19 +0900

    [Gold I] Title: 2048 (Easy), Time: 192 ms, Memory: 116204 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
@@ -4,149 +4,54 @@ def simulate(board, d):
     # d 방향으로 board 밀고 그 결과 반환하는 함수
     tmp_board = [[0] * N for _ in range(N)]
 
-    if d == 1:  # 우측으로 움직일 떄
-        for i in range(N):
-            idx = N - 1
-            tmp = deque()
-            for j in range(N - 1, -1, -1):
-                if board[i][j] != 0:
-                    tmp.append(board[i][j])
+    # 들어오는 d에 따라서 board 시계 방향으로 회전하면 되는 거 아닌감?
+    # 기준은 오른쪽으로 잡고, 2, 3, 4가 들어왔을 때의 board 형태를 바꿔준다면
 
-            if len(tmp) == 0:
-                continue
-            elif len(tmp) == 1:
-                num = tmp.pop()
-                tmp_board[i][idx] = num
-            else:
-                tmp_2 = []
-                while tmp:
-                    if len(tmp) == 1:
-                        num = tmp.pop()
-                        tmp_2.append(num)
-                        break
+    if d == 2 or d == 3:
+        board = list(map(list, zip(*board[::-1])))
+        if d == 3:
+            board = list(map(list, zip(*board[::-1])))
+    if d == 4:
+        board = list(map(list, zip(*board)))[::-1]
 
-                    if tmp[0] != tmp[1]:
-                        num = tmp.popleft()
-                        tmp_2.append(num)
+    for i in range(N):
+        idx = N - 1
+        tmp = deque()
+        for j in range(N - 1, -1, -1):
+            if board[i][j] != 0:
+                tmp.append(board[i][j])
 
-                    elif tmp[0] == tmp[1]:
-                        num = tmp.popleft()
-                        tmp_2.append(num * 2)
-                        tmp.popleft()  # 같은 숫자 두 개니까 한 번 더 빼줌..
+        if len(tmp) == 0:
+            continue
+        elif len(tmp) == 1:
+            num = tmp.pop()
+            tmp_board[i][idx] = num
+        else:
+            tmp_2 = []
+            while tmp:
+                if len(tmp) == 1:
+                    num = tmp.pop()
+                    tmp_2.append(num)
+                    break
 
-                for n in tmp_2:
-                    tmp_board[i][idx] = n
-                    idx -= 1
+                if tmp[0] != tmp[1]:
+                    num = tmp.popleft()
+                    tmp_2.append(num)
 
-    elif d == 2:  # 하측으로 움직일 떄
-        for j in range(N):
-            idx = N - 1
-            tmp = deque()
-            for i in range(N - 1, -1, -1):
-                if board[i][j] != 0:
-                    tmp.append(board[i][j])
+                elif tmp[0] == tmp[1]:
+                    num = tmp.popleft()
+                    tmp_2.append(num * 2)
+                    tmp.popleft()  # 같은 숫자 두 개니까 한 번 더 빼줌..
 
-            if len(tmp) == 0:
-                continue
-            elif len(tmp) == 1:
-                num = tmp.pop()
-                tmp_board[idx][j] = num
-            else:
-                tmp_2 = []
-                while tmp:
-                    if len(tmp) == 1:
-                        num = tmp.pop()
-                        tmp_2.append(num)
-                        break
-
-                    if tmp[0] != tmp[1]:
-                        num = tmp.popleft()
-                        tmp_2.append(num)
-
-                    elif tmp[0] == tmp[1]:
-                        num = tmp.popleft()
-                        tmp_2.append(num * 2)
-                        tmp.popleft()
-
-                for n in tmp_2:
-                    tmp_board[idx][j] = n
-                    idx -= 1
-
-    elif d == 3:  # 좌측으로 움직일 떄
-        for i in range(N):
-            idx = 0
-            tmp = deque()
-            for j in range(N):
-                if board[i][j] != 0:
-                    tmp.append(board[i][j])
-
-            if len(tmp) == 0:
-                continue
-            elif len(tmp) == 1:
-                num = tmp.pop()
-                tmp_board[i][idx] = num
-            else:
-                tmp_2 = []
-                while tmp:
-                    if len(tmp) == 1:
-                        num = tmp.pop()
-                        tmp_2.append(num)
-                        break
-
-                    if tmp[0] != tmp[1]:
-                        num = tmp.popleft()
-                        tmp_2.append(num)
-
-                    elif tmp[0] == tmp[1]:
-                        num = tmp.popleft()
-                        tmp_2.append(num * 2)
-                        tmp.popleft()  # 같은 숫자 두 개니까 한 번 더 빼줌..
-
-                for n in tmp_2:
-                    tmp_board[i][idx] = n
-                    idx += 1
-
-    else:  # 상측으로 움직일 때
-        for j in range(N):
-            idx = 0
-            tmp = deque()
-            for i in range(N):
-                if board[i][j] != 0:
-                    tmp.append(board[i][j])
-
-            if len(tmp) == 0:
-                continue
-            elif len(tmp) == 1:
-                num = tmp.pop()
-                tmp_board[idx][j] = num
-            else:
-                tmp_2 = []
-                while tmp:
-                    if len(tmp) == 1:
-                        num = tmp.pop()
-                        tmp_2.append(num)
-                        break
-
-                    if tmp[0] != tmp[1]:
-                        num = tmp.popleft()
-                        tmp_2.append(num)
-
-                    elif tmp[0] == tmp[1]:
-                        num = tmp.popleft()
-                        tmp_2.append(num * 2)
-                        tmp.popleft()
-
-                for n in tmp_2:
-                    tmp_board[idx][j] = n
-                    idx += 1
+            for n in tmp_2:
+                tmp_board[i][idx] = n
+                idx -= 1
 
     board = tmp_board
-    t_board = sum(board, [])
-    t_maxi = max(t_board)
-    return board, t_maxi
+    return board
 
 
-def make_combs(cnt, board, tmp_maxi):
+def make_combs(cnt, board):
     global maxi
 
     ttmp = sum(board, [])
@@ -160,13 +65,13 @@ def make_combs(cnt, board, tmp_maxi):
         return
 
     for i in range(1, 5):
-        calcul_arr, tmp_maxi = simulate(board, i)
-        make_combs(cnt+1, calcul_arr, tmp_maxi)
+        calcul_arr = simulate(board, i)
+        make_combs(cnt+1, calcul_arr)
 
 
 N = int(input())
 real_board = [list(map(int, input().split())) for _ in range(N)]
 dir = [1, 2, 3, 4]  # 상우하좌 순서
 maxi = 0
-make_combs(0, real_board, 0)
+make_combs(0, real_board)
 print(maxi)
