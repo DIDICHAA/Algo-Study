026eed40fa7d037181c0824aa32960ba419f874c
Jieun Seo <127473767+DIDICHAA@users.noreply.github.com>
2024-10-28 09:16:02 +0900

    [Gold IV] Title: 연구소, Time: 396 ms, Memory: 115832 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,66 +1,61 @@
-'''
-새로 세울 수 있는 벽의 개수는 3개, 이 3개를 반드시 세워야 함
--> 백트래킹으로 가능한 좌표의 조합 3개 만들어서 1 넣고 bfs 돌려서 max safe 구하기
-    -> 우선 좌표가 0인 애들 중에서 1을 세울 수 있는 좌표를 전부 넣고, 이 좌표 중 백트래킹으로 조합 만들기
-'''
 from collections import deque
-import copy
 
-def oob(i, j):
+def noob(i, j):
     return 0<=i<N and 0<=j<M
 
-def check_safe(visited, lab_copy):
-    global ans
-    q = deque()
 
-    for i in range(N):
-        for j in range(M):
-            if lab_copy[i][j] == 2:
-                q.append((i, j))
-                visited[i][j] = 1
-            if lab_copy[i][j] == 1:
-                visited[i][j] = 1
+def calcul(lst):
+    global ans
+
+    tmp = [row[:] for row in board]
+    for x, y in lst:
+        tmp[x][y] = 1
+
+    q = deque()
+    visited = [[0 for _ in range(M)] for _ in range(N)]
+    for x, y in virus:
+        q.append((x, y))
+        visited[x][y] = 1
 
     while q:
         ci, cj = q.popleft()
-        for di, dj in dir:
+        for di, dj in DIR:
             ni, nj = ci+di, cj+dj
-            if oob(ni, nj) and lab_copy[ni][nj] != 1 and visited[ni][nj] == 0:
+            if noob(ni, nj) and not visited[ni][nj] and tmp[ni][nj] == 0:
                 visited[ni][nj] = 1
                 q.append((ni, nj))
 
-
-    tmp = sum(visited, [])
-    tp_ans = tmp.count(0)
-    ans = max(ans, tp_ans)
-    return
+    cnt = 0
+    for i in range(N):
+        for j in range(M):
+            if visited[i][j] == 0 and tmp[i][j] == 0:
+                cnt += 1
+    ans = max(ans, cnt)
 
 
 def make_comb(cnt, idx, lst):
     if cnt == 3:
-        lab_copy = copy.deepcopy(lab)
-        for x, y in lst:
-            lab_copy[x][y] = 1
-
-        visited = [[0] * M for _ in range(N)]
-        check_safe(visited, lab_copy)
+        calcul(lst)
         return
 
-    for i in range(idx, len(none_lst)):
-        lst.append(none_lst[i])
+    for i in range(idx, len(candi)):
+        lst.append(candi[i])
         make_comb(cnt+1, i+1, lst)
         lst.pop()
 
 
 N, M = map(int, input().split())
-lab = [list(map(int, input().split())) for _ in range(N)]
-none_lst = []
+board = [list(map(int, input().split())) for _ in range(N)]
+DIR = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 ans = 0
-dir = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+candi = []
+virus = []
 for i in range(N):
     for j in range(M):
-        if lab[i][j] == 0:
-            none_lst.append((i, j))
+        if board[i][j] == 0:
+            candi.append([i, j])
+        elif board[i][j] == 2:
+            virus.append([i, j])
 
 make_comb(0, 0, [])
 print(ans)
