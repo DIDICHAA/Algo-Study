975bcabe2e18bf9096f67de2c3f9c7a360e8c84a
Jieun Seo <127473767+DIDICHAA@users.noreply.github.com>
2024-11-18 09:22:25 +0900

    [Gold V] Title: 토마토, Time: 4748 ms, Memory: 108216 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,36 +1,37 @@
 from collections import deque
 
+# BFS를 활용해 하루 단위로 익음을 전파
 def bfs():
-    q = deque()
-    v = [[0]*M for _ in range(N)]
-    cnt = 0
+    queue = deque()
+    visited = [[0 for _ in range(cols)] for _ in range(rows)]
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:  # 익은 토마토의 위치를 큐에 추가
+                queue.append((row, col))
+                visited[row][col] = 1
 
-    for i in range(N):
-        for j in range(M):
-            if tomato[i][j] == 1:
-                v[i][j] = 1
-                q.append((i, j))
-            elif tomato[i][j] == 0:
-                cnt += 1
+    while queue:
+        row, col = queue.popleft()
+        for i in range(4):
+            nxt_row, nxt_col = row + drs[i], col + dcs[i]
+            if not (0 <= nxt_row < rows and 0 <= nxt_col < cols): continue
+            if grid[nxt_row][nxt_col] != 0: continue
+            if visited[nxt_row][nxt_col] != 0: continue
+            visited[nxt_row][nxt_col] = visited[row][col] + 1
+            queue.append((nxt_row, nxt_col))
 
-    while q:
-        si, sj = q.popleft()
+    # 익지 않은 토마토가 남아있는지 확인
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == -1: continue
+            if visited[i][j] != 0: continue
+            return -1
+    return max(sum(visited, []))-1
 
-        for di, dj in dir:
-            ni, nj = si+di, sj+dj
-            if 0<=ni<N and 0<=nj<M and v[ni][nj] == 0 and tomato[ni][nj] == 0:
-                v[ni][nj] = v[si][sj] + 1
-                q.append((ni, nj))
-                cnt -= 1
-
-    if cnt > 0:
-        return -1
-    else:
-        return v[si][sj] - 1
-
-
-M, N = map(int, input().split())
-tomato = [list(map(int, input().split())) for _ in range(N)]
-dir = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+# 입력 처리
+cols, rows = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(rows)]
+drs = [0, 0, 1, -1]
+dcs = [1, -1, 0, 0]
 
 print(bfs())
