8eafe089b2d4a532144a40c9aa085b3f05c69c09
Jieun Seo <127473767+DIDICHAA@users.noreply.github.com>
2024-11-28 17:11:18 +0900

    [Gold V] Title: 신기한 소수, Time: 1464 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.py
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -1,9 +1,5 @@
 def check(lst):
-    temp_num = ''
-    for n in lst:
-        temp_num += str(n)
-
-    temp_num = int(temp_num)
+    temp_num = int(''.join(lst))
 
     for i in range(2, temp_num//2+1):
         if temp_num % i == 0:
@@ -13,16 +9,13 @@ def check(lst):
 
 def make_p(cnt, lst):
     if cnt == N:
-        new_num = ''
-        for n in lst:
-            new_num += str(n)
-        print(new_num)
+        print(''.join(lst))
         return
 
     for i in range(10):
         lst.append(nums[i])
 
-        if lst[0] == 0 or lst[0] == 1:
+        if lst[0] == '0' or lst[0] == '1':
             lst.pop()
             continue
 
@@ -35,5 +28,5 @@ def make_p(cnt, lst):
 
 
 N = int(input())
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+nums = '1234567890'
 make_p(0, [])
