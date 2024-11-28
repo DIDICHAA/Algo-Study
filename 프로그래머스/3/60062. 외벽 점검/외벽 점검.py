614d77f82c6ca7eafefa955362e035e4bccaca89
Jieun Seo <127473767+DIDICHAA@users.noreply.github.com>
2024-11-28 20:15:59 +0900

    [level 3] Title: 외벽 점검, Time: 1563.57 ms, Memory: 14.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.py
@@ -0,0 +1,27 @@
+from itertools import permutations
+
+
+def solution(n, weak, dist):
+    answer = 1e9
+
+    new_weak = weak[:]
+    for i in range(len(weak)):
+        new_weak.append(weak[i] + n)
+    cases = list(permutations(dist, len(dist)))
+
+    for s in range(len(weak)):
+        for case in cases:
+            cnt = 1
+            now = s
+            for i in range(1, len(weak)):
+                next = s+i
+                where = new_weak[next] - new_weak[now]
+                if where > case[cnt-1]:
+                    now = next
+                    cnt += 1
+                    if cnt > len(dist): break
+
+            if cnt <= len(dist):
+                answer = min(answer, cnt)
+
+    return answer if answer != 1e9 else -1
