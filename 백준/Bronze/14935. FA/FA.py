f069cbe9bbfa69e9cd17ca9e2ebfec9eed376145
Jieun Seo <127473767+DIDICHAA@users.noreply.github.com>
2025-07-23 22:50:11 +0900

    [Bronze IV] Title: FA, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/14935. FA/FA.py
+++ 백준/Bronze/14935. FA/FA.py
@@ -1,15 +1,2 @@
 x = int(input())
-lst = list(map(int, str(x)))
-leng = len(lst)
-
-x = -1
-flag = False
-while 1:
-    num = lst[0] * leng
-    if x != num : x = num
-    else:
-        flag = True
-        break
-
-if flag: print('FA')
-else: print('NFA')
+print('FA')
