d712c332f537400b054ca7c8321f5ced23865e95
Jieun Seo <127473767+DIDICHAA@users.noreply.github.com>
2025-07-27 01:51:48 +0900

    [Gold IV] Title: 연구소, Time: 892 ms, Memory: 116260 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,61 +1,58 @@
 from collections import deque
 
-def noob(i, j):
+def iob(i, j):
     return 0<=i<N and 0<=j<M
 
-
-def calcul(lst):
-    global ans
-
-    tmp = [row[:] for row in board]
-    for x, y in lst:
-        tmp[x][y] = 1
+def find_safe_zone(c1, c2, c3):
+    global safe_zone
 
     q = deque()
-    visited = [[0 for _ in range(M)] for _ in range(N)]
-    for x, y in virus:
-        q.append((x, y))
-        visited[x][y] = 1
+    visited = set()
+    virus = set()
+
+    for i in range(N):
+        for j in range(M):
+            if board[i][j] != 2: continue
+            q.append((i, j))
+            visited.add((i, j))
+            virus.add((i, j))
 
     while q:
         ci, cj = q.popleft()
         for di, dj in DIR:
             ni, nj = ci+di, cj+dj
-            if noob(ni, nj) and not visited[ni][nj] and tmp[ni][nj] == 0:
-                visited[ni][nj] = 1
-                q.append((ni, nj))
+            if not iob(ni, nj): continue
+            if (ni, nj) in visited: continue
+            if board[ni][nj] == 1: continue
+            if (ni, nj) == c1 or (ni, nj) == c2 or (ni, nj) == c3: continue
+            virus.add((ni, nj))
+            q.append((ni, nj))
+            visited.add((ni, nj))
 
-    cnt = 0
-    for i in range(N):
-        for j in range(M):
-            if visited[i][j] == 0 and tmp[i][j] == 0:
+    if virus:
+        cnt = 0
+        for i in range(N):
+            for j in range(M):
+                if board[i][j] != 0: continue
+                if (i, j) in virus: continue
+                if (i, j) == c1 or (i, j) == c2 or (i, j) == c3: continue
                 cnt += 1
-    ans = max(ans, cnt)
-
-
-def make_comb(cnt, idx, lst):
-    if cnt == 3:
-        calcul(lst)
-        return
-
-    for i in range(idx, len(candi)):
-        lst.append(candi[i])
-        make_comb(cnt+1, i+1, lst)
-        lst.pop()
+        safe_zone = max(safe_zone, cnt)
 
 
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
-DIR = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-ans = 0
-candi = []
-virus = []
+DIR = [(-1,  0), (0, 1), (1, 0), (0, -1)]
+
+candidate = []
+safe_zone = -1
 for i in range(N):
     for j in range(M):
-        if board[i][j] == 0:
-            candi.append([i, j])
-        elif board[i][j] == 2:
-            virus.append([i, j])
+        if board[i][j] != 0: continue
+        candidate.append((i, j))
 
-make_comb(0, 0, [])
-print(ans)
+for i in range(0, len(candidate)-2):
+    for j in range(i+1, len(candidate)-1):
+        for k in range(j+1, len(candidate)):
+            find_safe_zone(candidate[i], candidate[j], candidate[k])
+print(safe_zone)
